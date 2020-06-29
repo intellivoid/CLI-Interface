f39e1db6f276946f75566afe25545a88825c5d06
netkas <netkas@intellivoid.info>
2020-06-29 16:02:08 -0400

    Added project files

---
+++ Views/Clipboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Intellivoid.NStack;
+
+namespace Intellivoid.cli {
+	/// <summary>
+	/// Provides cut, copy, and paste support for the clipboard. 
+	/// NOTE: Currently not implemented.
+	/// </summary>
+	public static class Clipboard {
+		/// <summary>
+		/// 
+		/// </summary>
+		public static ustring Contents { get; set; }
+	}
+}
